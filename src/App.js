7ebbc78847840bdb73246d8c5931f17f1f96c42b
Binyam Sintayehu <73512283+Binyam-maker@users.noreply.github.com>
2022-06-23 09:14:54 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar, Sidebar, Footer } from "./components";
+import {
+  Home,
+  About,
+  Products,
+  SingleProducts,
+  Cart,
+  Checkout,
+  Error,
+  PrivateRoute,
+  AuthWrapper,
+} from "./pages";
+
+function App() {
+  return (
+    <AuthWrapper>
+      <Router>
+        <Navbar />
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="cart" element={<Cart />} />
+          <Route path="products" element={<Products />} />
+          <Route path="products/:id" element={<SingleProducts />} />
+          <Route
+            path="checkout"
+            element={
+              <PrivateRoute>
+                <Checkout />
+              </PrivateRoute>
+            }
+          />
+          <Route path="*" element={<Error />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </AuthWrapper>
+  );
+}
+
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import Home from "./HomePage";
+import About from "./AboutPage";
+import Products from "./ProductsPage";
+import SingleProducts from "./SingleProductPage";
+import Cart from "./CartPage";
+import Checkout from "./CheckoutPage";
+import Error from "./ErrorPage";
+import PrivateRoute from "./PrivateRoute";
+import AuthWrapper from "./AuthWrapper";
+
+export {
+  Home,
+  About,
+  Products,
+  SingleProducts,
+  Cart,
+  Checkout,
+  Error,
+  PrivateRoute,
+  AuthWrapper,
+};
